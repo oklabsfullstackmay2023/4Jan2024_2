9f482682b39f7cb125e3a9b093a8584b19853a0c
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2024-01-05 14:17:52 +0530

    'concatination_operator'

---
+++ cancatination.py
@@ -0,0 +1,3 @@
+
+
+print("Why"+" "+"ban"+" "+'meat' +" "+'when' + ' '+ 'lord' +' '+'ram' +' '+'was' +' '+'a' +' '+'non' +' '+'vegetarian,' +' '+'says' +' '+'NCP' +' '+'leader'+' '+'Jitendra' +' '+'Awhad;' +' '+'BJP' +' '+'hits' +' '+'back')
---
+++ concatination_operator.py
@@ -0,0 +1,6 @@
+name="Anil"
+surname="Dollor"
+
+print(name+surname)
+print(3+4)
+print(2.5+2.5)
